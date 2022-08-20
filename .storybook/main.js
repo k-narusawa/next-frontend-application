e19df96f6dc68824b60e855375da5c82c8044356
k-narusawa <swkei320@gmail.com>
2022-08-20 20:03:59 +0900

    add: storybookのパスの設定を修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
 const path = require('path')
 
 module.exports = {
-  stories: ['../src/components/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: [
+    '../src/components/**/*.stories.@(ts|tsx|js|jsx|mdx)'
+  ],
+  addons: [
+    '@storybook/addon-links', 
+    '@storybook/addon-essentials'
+  ],
+
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
@@ -20,8 +26,12 @@ module.exports = {
       ],
       include: path.resolve(__dirname, '../'),
     })
+    config.resolve.alias = {
+      'components': path.resolve(__dirname, '../src/components'),
+    }
     return config
   },
+
   core: {
     builder: 'webpack5',
   },
