f2082f3f8ff97171146645488351238fbb38f544
k-narusawa <swkei320@gmail.com>
2022-08-21 10:38:45 +0900

    add: _document.tsxの追加

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+
+class SampleDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="dns-prefetch" href="//www.google.co.jp" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default SampleDocument
