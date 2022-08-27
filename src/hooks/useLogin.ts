16a135503000d5ad2cdbb78a6aa684b3e495710d
k-narusawa <swkei320@gmail.com>
2022-08-27 11:22:07 +0900

    add: loginのhookを追加

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,52 @@
+import { apiClient } from 'lib/apiClient'
+import { useRouter } from 'next/router'
+import { useCallback, useState } from 'react'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { loginSelector } from 'states/selectors/loginSelector'
+
+export const useLogin = () => {
+  const setIsLogin = useSetRecoilState(loginSelector)
+  const [isLoading, setIsLoading] = useState(false)
+  const isLogin = useRecoilValue(loginSelector)
+  const router = useRouter()
+
+  const login = useCallback(
+    async(
+      loginId: string,
+      password: string
+    ) => {
+    //   const data = {loginId: loginId, password: password}
+    //   await apiClient.post<login>("/api/login", data)
+    //   .then((response) => {
+    //     setIsLogin(true)
+    //     return {
+    //       accessToken: response.data.accessToken,
+    //       refreshToken: response.data.refreshToken
+    //     }
+    //   })
+    //   .catch((e) => {
+    //     setIsLogin(false)
+    //     console.log(e)
+    //   })
+      setIsLogin(true)
+      setIsLoading(false)
+      router.push("/")
+    },[setIsLoading],
+  )
+
+  const logout = useCallback(
+    () => {
+      console.log('ログアウト')
+      setIsLogin(false)
+      setIsLoading(false)
+    },
+    [],
+  )
+
+  return {
+    isLoading,
+    isLogin,
+    login,
+    logout
+  }
+}
