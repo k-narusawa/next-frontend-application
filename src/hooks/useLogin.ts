3df72e3e2a1bc7f5f758662176d78c755c5c387e
k-narusawa <swkei320@gmail.com>
2022-08-30 07:45:34 +0900

    fix: loginのhookを修正

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,4 +1,4 @@
-import { AxiosError } from "axios";
+import axios, { AxiosError } from "axios";
 import { apiClient } from "lib/apiClient";
 import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
@@ -16,66 +16,78 @@ export const useLogin = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
 
-  const login = useCallback(
-    async (loginId: string, password: string) => {
-      const data = { loginId: loginId, password: password };
-
-      await apiClient
-        .post<token>("/api/login", data)
-        .then((response) => {
-          setIsLogin(true);
-          setToken({
-            accessToken: response.data.accessToken,
-            refreshToken: response.data.refreshToken,
-          });
-          router.push("/");
-        })
-        // TODO エラーハンドリング考え直す
-        .catch((e: AxiosError<error>) => {
-          setIsLogin(false);
-          if (e.response?.data.errorCode === "UN_AUTHORIZED") {
-            setErrorMessage("認証に失敗しました");
-          } else if (e.response?.data.errorCode === "SERVER_ERROR") {
-            setErrorMessage("サーバーエラーが発生しました");
-          } else {
-            setErrorMessage("予期せぬエラーが発生しました");
-          }
+  const login = useCallback(async (loginId: string, password: string) => {
+    const data = { loginId: loginId, password: password };
+    setIsLoading(true);
+    try {
+      await apiClient.post<token>("/api/login", data).then((response) => {
+        setIsLogin(true);
+        setToken({
+          accessToken: response.data.accessToken,
+          refreshToken: response.data.refreshToken,
         });
-
+        router.push("/");
+      });
+    } catch (e: any) {
+      // TODO エラーハンドリング考え直す
+      setIsLogin(false);
+      if (
+        axios.isAxiosError(e) &&
+        (e as AxiosError<error>).response?.data.status &&
+        (e as AxiosError<error>).response?.data.message &&
+        (e as AxiosError<error>).response?.data.errorCode
+      ) {
+        // TODO ifのネスト読みにくすぎるのでeslintを書き直す
+        if (
+          (e as AxiosError<error>).response?.data.errorCode === "UN_AUTHORIZED"
+        ) {
+          setErrorMessage("認証に失敗しました！");
+        } else if (
+          (e as AxiosError<error>).response?.data.errorCode === "SERVER_ERROR"
+        ) {
+          setErrorMessage("サーバーエラーが発生しました！");
+        } else {
+          setErrorMessage("予期せぬエラーが発生しました！");
+        }
+      } else {
+        setErrorMessage("予期せぬエラーが発生しました！");
+      }
+    } finally {
       setIsLoading(false);
-    },
-    [setIsLoading]
-  );
+    }
+  }, []);
 
   const logout = useCallback(async () => {
     const config = {
+      // TODO headerの持たせ方を共通化させる
       headers: {
         Authorization: "Bearer " + token.accessToken,
       },
     };
-    await apiClient
-      .post("/api/logout", undefined, config)
-      .then(() => {
+    setIsLoading(true);
+    try {
+      await apiClient.post("/api/logout", undefined, config).then(() => {
         setIsLogin(true);
         setToken({
           accessToken: "",
           refreshToken: "",
         });
         router.push("/");
-      })
-      // TODO エラーハンドリング考え直す
-      .catch((e: AxiosError<error>) => {
-        console.log(e);
-        setIsLogin(false);
-        setToken({
-          accessToken: "",
-          refreshToken: "",
-        });
-        router.push("/");
       });
-    setIsLogin(false);
-    setIsLoading(false);
-  }, [setIsLoading]);
+    } catch (e: any) {
+      // TODO エラーハンドリング考え直す
+      console.log(e);
+      setIsLogin(false);
+      setToken({
+        accessToken: "",
+        refreshToken: "",
+      });
+      router.push("/");
+    } finally {
+      setIsLogin(false);
+      setIsLoading(false);
+    }
+  }, []);
 
   return {
     isLoading,
