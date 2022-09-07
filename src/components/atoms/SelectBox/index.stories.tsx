82188e9f89a8ff8ad54448ac573876f557a3ba51
k-narusawa <swkei320@gmail.com>
2022-09-08 07:34:05 +0900

    add: selectboxのコンポーネント実装

---
+++ src/components/atoms/SelectBox/index.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import SelectBox from "./index";
+
+export default {
+  title: "atoms/SelectBox",
+  component: SelectBox,
+} as ComponentMeta<typeof SelectBox>;
+
+const Template: ComponentStory<typeof SelectBox> = (args) => <SelectBox {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  options: ["HIGH", "MIDDLE", "LOW"]
+};
